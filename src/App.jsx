d65689e92a3d3fc84019a7e9d457ba0f32ec81e0
Alvaro-Ezequiel-Zalbaveascoba <azalbaveascoba@hotmail.com>
2022-04-14 07:05:49 -0300

    Demo de abril

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,44 @@ import NavBar from "./components/NavBar";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
-import { Box, Center, Flex } from "@chakra-ui/react";
+import { Box, Center} from "@chakra-ui/react";
 import DetailsHouse from "./commons/DetailsHouse";
 import CheckOut from "./commons/CheckOut";
+import Accomplished from "./commons/Accomplished";
+import Footer from "./components/Footer";
+import Users from "./commons/Users";
+import Messages from "./commons/Messages";
 
 function App() {
   return (
-    <Box>
-      <Flex>
-        <Box>
+    <Box position="absolute " w="100%">
+
+        
           <NavBar />
-        </Box>
-      </Flex>
+      
+      
       <Box  >
         <Center>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/checkout" element={<CheckOut/>}/>
-          
-            <Route path="/houses/:id" element={<DetailsHouse/>}/>
+            <Route path="/checkout" element={<CheckOut />} />
+            <Route path="/accomplished" element={<Accomplished />} />
+            <Route path="/users"element={<Users />} />
+            <Route path="/messages"element={<Messages />} />
+
+            <Route path="/houses/:id" element={<DetailsHouse />} />
 
 
 
-            <Route path="/casas"element={<Home/>} />
-            <Route path="/ph"element={<Home/>} />
+            <Route path="/casas" element={<Home />} />
+            <Route path="/ph" element={<Home />} />
             <Route path="/" element={<Home />} />
             <Route path="*" element={<Navigate replace to="/" />} />
           </Routes>
         </Center>
       </Box>
+      <Footer />
     </Box>
   );
 }
